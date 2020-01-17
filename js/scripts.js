7c3e197af28df7a95fd0df87b8e97c102b906439
Jozy Kinnaman <jozypants@gmail.com>
2020-01-17 15:59:25 -0800

    add css style, id's to html, and and else statement comments to js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
 $(document).ready(function() {
+  $("form").submit(function(event) {
+   pro =$("option#").val();
+    
+ console.log(pro) 
+
+//   if ( === "no" &&  === "yes") {
+    
+//     $("#").show();
+// } else if ( === "no" &&  === "no") {
+   
+//     $("#").show();
+// } else if ( === "yes" &&  === "no") {
+  
+//     $("").show();
+// } else {
+//     $("").show();
+// }
+
+event.preventDefault();
+});
+});
